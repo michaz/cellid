2539d27d5ec322676c6d20524486bf0f91c79635
Michael Zilske <michael.zilske@tu-berlin.de>
2016-04-09 15:17:54 +0200

    Initial commit

---
+++ app/src/main/java/org/michaz/cellid/CellIdService.java
@@ -0,0 +1,120 @@
+package org.michaz.cellid;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class CellIdService extends Service implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    /**
+     * Class for clients to access.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with
+     * IPC.
+     */
+    public class LocalBinder extends Binder {
+        CellIdService getService() {
+            return CellIdService.this;
+        }
+    }
+
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .addApi(ActivityRecognition.API)
+                .build();
+        mGoogleApiClient.connect();
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    // This is the object that receives interactions from clients.  See
+    // RemoteService for a more complete example.
+    private final IBinder mBinder = new LocalBinder();
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(
+                mGoogleApiClient,
+                10000,
+                getActivityDetectionPendingIntent()
+        );
+    }
+
+    void turnOnLocationUpdates() {
+        if (mGoogleApiClient.isConnected()) {
+            LocationRequest lr = new LocationRequest();
+            lr.setInterval(10000);
+            lr.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    mGoogleApiClient,
+                    lr,
+                    getLocationUpdatePendingIntent()
+            );
+        }
+    }
+
+    void turnOffLocationUpdates() {
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(
+                    mGoogleApiClient,
+                    getLocationUpdatePendingIntent()
+            );
+        }
+    }
+
+    private PendingIntent getLocationUpdatePendingIntent() {
+        Intent intent = new Intent(this, LocationUpdateIntentService.class);
+
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+        // requestActivityUpdates() and removeActivityUpdates().
+        return PendingIntent.getService(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private PendingIntent getActivityDetectionPendingIntent() {
+        Intent intent = new Intent(this, DetectedActivitiesIntentService.class);
+
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+        // requestActivityUpdates() and removeActivityUpdates().
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onDestroy() {
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+}
---
+++ app/src/main/java/org/michaz/cellid/LocationUpdateIntentService.java
@@ -0,0 +1,98 @@
+package org.michaz.cellid;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.location.Location;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.NeighboringCellInfo;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import com.firebase.client.Firebase;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationResult;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+
+public class LocationUpdateIntentService extends IntentService {
+
+
+    private TelephonyManager tm;
+    private GoogleApiClient mGoogleApiClient;
+
+    public LocationUpdateIntentService() {
+        super("LocationUpdateIntentService");
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        tm = (TelephonyManager) getApplicationContext().getSystemService(TELEPHONY_SERVICE);
+        if (tm == null) {
+            Log.e("LocationUpdate", "onStart: tm == null");
+        } else {
+            Log.i("LocationUpdate", "onStart: tm initialized");
+        }
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.i("locationUpdate", "Location update.");
+        LocationResult locationResult = LocationResult.extractResult(intent);
+        if (locationResult != null) {
+            List<CellInfo> allCellInfo = tm.getAllCellInfo();
+            if (allCellInfo != null) { // happens!
+                for (CellInfo cellInfo : allCellInfo) {
+                    Log.i("allCellInfo", cellInfo.toString());
+                    if (cellInfo.isRegistered()) {
+                        if (cellInfo instanceof CellInfoLte) {
+                            Firebase newCellUpdate = new Firebase("https://brilliant-fire-9861.firebaseio.com/cellInfoLte")
+                                    .push();
+                            HashMap<String, Object> values = new HashMap<>();
+
+                            values.put("timestamp", Calendar.getInstance().getTimeInMillis());
+                            CellInfoLte cellInfoLte = (CellInfoLte) cellInfo;
+                            values.put("deviceId", tm.getDeviceId());
+                            values.put("ci", cellInfoLte.getCellIdentity().getCi());
+                            values.put("mcc", cellInfoLte.getCellIdentity().getMcc());
+                            values.put("mnc", cellInfoLte.getCellIdentity().getMnc());
+                            values.put("pci", cellInfoLte.getCellIdentity().getPci());
+                            values.put("tac", cellInfoLte.getCellIdentity().getTac());
+                            Location lastLocation = locationResult.getLastLocation();
+                            values.put("lat", lastLocation.getLatitude());
+                            values.put("lng", lastLocation.getLongitude());
+                            newCellUpdate.setValue(values);
+                            Log.i("locationUpdate", "Logged Lte.");
+                        } else if (cellInfo instanceof CellInfoGsm) {
+                            Firebase newCellUpdate = new Firebase("https://brilliant-fire-9861.firebaseio.com/cellInfoGsm")
+                                    .push();
+                            newCellUpdate.child("timestamp").setValue(Calendar.getInstance().getTimeInMillis());
+                            CellInfoGsm cellInfoGsm = (CellInfoGsm) cellInfo;
+                            newCellUpdate.child("deviceId").setValue((tm.getDeviceId()));
+                            newCellUpdate.child("mcc").setValue((cellInfoGsm.getCellIdentity().getMcc()));
+                            newCellUpdate.child("mnc").setValue((cellInfoGsm.getCellIdentity().getMnc()));
+                            newCellUpdate.child("lac").setValue((cellInfoGsm.getCellIdentity().getLac()));
+                            Location lastLocation = locationResult.getLastLocation();
+                            newCellUpdate.child("lat").setValue(lastLocation.getLatitude());
+                            newCellUpdate.child("lng").setValue(lastLocation.getLongitude());
+                            Log.i("locationUpdate", "Logged Gsm.");
+                        }
+                    }
+                }
+            }
+        }
+        List<NeighboringCellInfo> neighboringCellInfo = tm.getNeighboringCellInfo();
+        if (neighboringCellInfo != null) {
+            for (NeighboringCellInfo cellInfo : neighboringCellInfo) {
+                Log.i("neighboringCellInfo", cellInfo.toString());
+            }
+        }
+    }
+
+}
